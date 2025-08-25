6bc69a1623ef2d09bb9dcc63db2f75bbf3fa2f1d
aysegultoptas <aysegulltoptass@gmail.com>
2025-08-25 23:22:27 +0300

    mentor

--- sources/pushing.c
+++ sources/pushing.c
@@ -1,6 +1,17 @@
 #include "push_swap.h"
+#include "libft/libft.h"
 
 void sa(t_stack *stack_a)
 {
+    t_node *first;
+    t_node *second;
+    if (stack_a == NULL || stack_a->size < 2)
+        return;
+    first = stack_a->top;
+    second = first->next;
+    first->next = second->next;
+    second->next = first;
+    stack_a->top = second;
 
+    write(1, "sa\n", 3);
 }
